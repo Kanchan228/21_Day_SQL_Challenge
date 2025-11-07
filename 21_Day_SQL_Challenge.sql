4cb39741a5d73f7688331ec6dcd7fe55bfeea60b
Kanchan <166362725+Kanchan228@users.noreply.github.com>
2025-11-07 23:36:15 +0530

    Add SQL queries for patient and staff statistics

--- 21_Day_SQL_Challenge.sql
+++ 21_Day_SQL_Challenge.sql
@@ -176,3 +176,33 @@ SUM(patients_admitted) as total_patient,
 SUM(patients_refused) as total_patient_refused,
 ROUND (AVG(patient_satisfaction),2) AS avg_patient_satisfaction
 FROM services_weekly;
+
+# Day 6
+-- 1. Count the number of patients by each service.
+SELECT service,
+count(patient_id) as total_patient
+FROM patients
+GROUP BY service;
+
+-- 2. Calculate the average age of patients grouped by service.
+SELECT service,
+ROUND(AVG(age),2) AS Avg_age
+FROM patients
+GROUP BY service;
+
+-- 3. Find the total number of staff members per role.
+SELECT role,
+COUNT(staff_id) as total_staff_members
+FROM staff
+GROUP BY role;
+
+/* Challenge Question: For each hospital service, calculate the total number of patients admitted, total patients 
+refused, and the admission rate (percentage of requests that were admitted). Order by admission rate descending. */
+
+SELECT service,
+SUM(patients_admitted) as total_patient,
+SUM(patients_refused) as total_patient_refused,
+ROUND(SUM(patients_admitted)*100/ SUM(patients_request),2) AS admission_rate
+FROM services_weekly
+GROUP BY service
+ORDER BY admission_rate;
