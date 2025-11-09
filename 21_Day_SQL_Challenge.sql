691887fa47ede3375bb1670d6375dd80d46896bb
Kanchan <166362725+Kanchan228@users.noreply.github.com>
2025-11-09 15:54:32 +0530

    Add SQL queries for patient and staff analysis

--- 21_Day_SQL_Challenge.sql
+++ 21_Day_SQL_Challenge.sql
@@ -206,3 +206,41 @@ ROUND(SUM(patients_admitted)*100/ SUM(patients_request),2) AS admission_rate
 FROM services_weekly
 GROUP BY service
 ORDER BY admission_rate;
+
+# Day 7
+-- 1. Find services that have admitted more than 500 patients in total.
+SELECT service,
+		SUM(patients_admitted) AS total_patient
+ FROM services_weekly
+ GROUP BY  service
+ HAVING sum(patients_admitted)>500;
+ 
+-- 2. Show services where average patient satisfaction is below 75.
+SELECT
+		service,
+        AVG(patient_satisfaction) AS avg_satisfaction
+FROM services_weekly
+GROUP BY service
+HAVING AVG(patient_satisfaction)<75;
+
+-- 3. List weeks where total staff presence across all services was less than 50.
+
+SELECT 
+    week, SUM(present) AS total_staff_present
+FROM staff_schedule
+GROUP BY week
+HAVING SUM(present) < 50;
+
+/*Question: Identify services that refused more than 100 patients in total and had an average patient satisfaction
+ below 80. Show service name, total refused, and average satisfaction.*/
+ 
+SELECT 
+		service,
+        SUM(patients_refused) AS total_patients,
+        Round(AVG(patient_satisfaction),2) AS avg_satisfaction
+FROM services_weekly
+GROUP BY service
+HAVING SUM(patients_refused)>100 AND
+AVG(patient_satisfaction)<80 ;
+
+
