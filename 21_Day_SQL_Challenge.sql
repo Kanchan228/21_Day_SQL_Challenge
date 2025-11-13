5e39e98e76f93a05e7ba038056799a2f12867b07
Kanchan <166362725+Kanchan228@users.noreply.github.com>
2025-11-13 22:34:35 +0530

    Add SQL queries for unique services and roles
    Added SQL queries to list unique services, staff roles, and distinct months, along with a challenge question for unique service and event combinations.

--- 21_Day_SQL_Challenge.sql
+++ 21_Day_SQL_Challenge.sql
@@ -329,3 +329,30 @@ END as Performance_category
 FROM services_weekly
 GROUP BY service
 ORDER BY AVG(patient_satisfaction) DESC;
+
+# Day 11
+-- 1. List all unique services in the patients table.
+SELECT DISTINCT service
+FROM services_weekly;
+
+-- 2. Find all unique staff roles in the hospital.
+SELECT 
+DISTINCT role
+FROM staff;
+
+-- 3. Get distinct months from the services_weekly table
+SELECT
+DISTINCT month
+FROM services_weekly;
+
+/*Challenge Question: Find all unique combinations of service and event type from the services_weekly table where 
+events are not null or none, along with the count of occurrences for each combination. Order by count descending. */
+
+SELECT 
+		service, event,
+		COUNT(*) AS occurrence
+FROM services_weekly
+WHERE event is not null and
+	  event <> 'none'
+GROUP BY service, event
+ORDER BY occurrence DESC;
