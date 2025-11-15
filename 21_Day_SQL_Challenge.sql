a98eac9c7be6619f85aba7db80f6015f4d91024c
Kanchan <166362725+Kanchan228@users.noreply.github.com>
2025-11-15 17:07:07 +0530

    Update 21_Day_SQL_Challenge.sql

--- 21_Day_SQL_Challenge.sql
+++ 21_Day_SQL_Challenge.sql
@@ -356,3 +356,43 @@ WHERE event is not null and
 	  event <> 'none'
 GROUP BY service, event
 ORDER BY occurrence DESC;
+
+# Day 12
+-- 1. Find all weeks in services_weekly where no special event occurred.
+SELECT 
+	  DISTINCT week
+FROM services_weekly
+WHERE event = 'none' OR
+	  event IS NULL;
+
+-- 2. Count how many records have null or empty event values.
+SELECT 
+    COUNT(*) AS null_or_empty_events
+FROM services_weekly
+WHERE event IS NULL 
+      OR event = '';
+
+-- 3. List all services that had at least one week with a special event.
+SELECT
+      DISTINCT service
+FROM services_weekly
+WHERE event is not null AND
+	 event !='none' AND
+	 event != '';
+     
+/* Challlenge Question: Analyze the event impact by comparing weeks with events vs weeks without events.
+Show: event status ('With Event' or 'No Event'), count of weeks, average patient satisfaction, and average staff 
+morale. Order by average patient satisfaction descending. */     
+ 
+ SELECT 
+ CASE WHEN
+      event IS NOT NULL AND 
+      event!= '' AND
+      event != 'none' THEN 'with_event' 
+      ELSE 'without_event'
+ END AS event_status,
+     COUNT(DISTINCT week) AS week_count,
+     ROUND(AVG(patient_satisfaction),2) as avg_score,
+     ROUND(AVG(staff_morale),2) as avg_staff_morale
+ FROM services_weekly
+ GROUP BY event_status;
